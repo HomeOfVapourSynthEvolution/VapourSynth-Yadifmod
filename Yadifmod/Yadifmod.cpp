e6a49a24b1b8c5635e0351131f1cd8d79a4a1a2f
HolyWu <holywu@gmail.com>
2014-08-15 22:22:15 +0800

    Add vector operations

--- Yadifmod/Yadifmod.cpp
+++ Yadifmod/Yadifmod.cpp
@@ -23,6 +23,9 @@
 */
 
 #include <cstdlib>
+#ifdef VS_TARGET_CPU_X86
+#include "./vectorclass/vectorclass.h"
+#endif
 #include "VapourSynth.h"
 #include "VSHelper.h"
 
@@ -59,6 +62,78 @@ static void YadifmodC(const T *prev2pp, const T *prev2pn, const T *prevp2p, cons
                 dstp[x] = p2 - diff;
             else
                 dstp[x] = spatialPred;
+        }
+        prev2pp += stride;
+        prev2pn += stride;
+        prevp2p += stride;
+        prevp += stride;
+        prevp2n += stride;
+        srcpp += stride;
+        srcpn += stride;
+        nextp2p += stride;
+        nextp += stride;
+        nextp2n += stride;
+        next2pp += stride;
+        next2pn += stride;
+        edeintp += stride;
+        dstp += stride;
+    }
+}
+
+template<typename T, typename V1, typename V2, int VectorSize>
+static void YadifmodSIMD(const T *prev2pp, const T *prev2pn, const T *prevp2p, const T *prevp, const T *prevp2n, const T *srcpp, const T *srcpn, const T *nextp2p, const T *nextp, const T *nextp2n, const T *next2pp, const T *next2pn, const T *edeintp, T *dstp, int width, int starty, int stopy, int stride, const YadifmodData *d) {
+    for (int y = starty; y <= stopy; y += 2) {
+        for (int x = 0; x < width; x += VectorSize) {
+            V1 prev2ppV = V1().load_a(prev2pp + x);
+            V1 prev2pnV = V1().load_a(prev2pn + x);
+            V1 prevp2pV = V1().load_a(prevp2p + x);
+            V1 prevpV = V1().load_a(prevp + x);
+            V1 prevp2nV = V1().load_a(prevp2n + x);
+            V1 srcppV = V1().load_a(srcpp + x);
+            V1 srcpnV = V1().load_a(srcpn + x);
+            V1 nextp2pV = V1().load_a(nextp2p + x);
+            V1 nextpV = V1().load_a(nextp + x);
+            V1 nextp2nV = V1().load_a(nextp2n + x);
+            V1 next2ppV = V1().load_a(next2pp + x);
+            V1 next2pnV = V1().load_a(next2pn + x);
+            V1 edeintpV = V1().load_a(edeintp + x);
+            if (width - x < VectorSize) {
+                prev2ppV.cutoff(width - x);
+                prev2pnV.cutoff(width - x);
+                prevp2pV.cutoff(width - x);
+                prevpV.cutoff(width - x);
+                prevp2nV.cutoff(width - x);
+                srcppV.cutoff(width - x);
+                srcpnV.cutoff(width - x);
+                nextp2pV.cutoff(width - x);
+                nextpV.cutoff(width - x);
+                nextp2nV.cutoff(width - x);
+                next2ppV.cutoff(width - x);
+                next2pnV.cutoff(width - x);
+                edeintpV.cutoff(width - x);
+            }
+
+            V2 p1 = V2(extend_low(srcppV), extend_high(srcppV));
+            V2 p2 = (V2(extend_low(prevpV), extend_high(prevpV)) + V2(extend_low(nextpV), extend_high(nextpV))) >> 1;
+            V2 p3 = V2(extend_low(srcpnV), extend_high(srcpnV));
+            V2 tdiff0 = abs(V2(extend_low(prevpV), extend_high(prevpV)) - V2(extend_low(nextpV), extend_high(nextpV)));
+            V2 tdiff1 = (abs(V2(extend_low(prev2ppV), extend_high(prev2ppV)) - p1) + abs(V2(extend_low(prev2pnV), extend_high(prev2pnV)) - p3)) >> 1;
+            V2 tdiff2 = (abs(V2(extend_low(next2ppV), extend_high(next2ppV)) - p1) + abs(V2(extend_low(next2pnV), extend_high(next2pnV)) - p3)) >> 1;
+            V2 diff = max(max(tdiff0 >> 1, tdiff1), tdiff2);
+            if (d->mode < 2) {
+                V2 p0 = (V2(extend_low(prevp2pV), extend_high(prevp2pV)) + V2(extend_low(nextp2pV), extend_high(nextp2pV))) >> 1;
+                V2 p4 = (V2(extend_low(prevp2nV), extend_high(prevp2nV)) + V2(extend_low(nextp2nV), extend_high(nextp2nV))) >> 1;
+                V2 maxs = max(max(p2 - p3, p2 - p1), min(p0 - p1, p4 - p3));
+                V2 mins = min(min(p2 - p3, p2 - p1), max(p0 - p1, p4 - p3));
+                diff = max(max(diff, mins), -maxs);
+            }
+            V2 spatialPred = V2(extend_low(edeintpV), extend_high(edeintpV));
+            V2 result = select(spatialPred > p2 + diff, p2 + diff, select(spatialPred < p2 - diff, p2 - diff, spatialPred));
+
+            if (width - x >= VectorSize)
+                V1(compress(result.get_low(), result.get_high())).store_a(dstp + x);
+            else
+                V1(compress(result.get_low(), result.get_high())).store_partial(width - x, dstp + x);
         }
         prev2pp += stride;
         prev2pn += stride;
@@ -150,9 +225,17 @@ static const VSFrameRef *VS_CC yadifmodGetFrame(int n, int activationReason, voi
                 const uint8_t * next2pn = next2pp + stride3;
 
                 if (d->vi.format->bytesPerSample == 1) {
+#ifdef VS_TARGET_CPU_X86
+                    YadifmodSIMD<uint8_t, Vec16uc, Vec16s, 16>(prev2pp, prev2pn, prevp2p, prevp, prevp2n, srcpp, srcpn, nextp2p, nextp, nextp2n, next2pp, next2pn, edeintp, dstp, width, starty, stopy, stride3, d);
+#else
                     YadifmodC<uint8_t>(prev2pp, prev2pn, prevp2p, prevp, prevp2n, srcpp, srcpn, nextp2p, nextp, nextp2n, next2pp, next2pn, edeintp, dstp, width, starty, stopy, stride3, d);
+#endif
                 } else if (d->vi.format->bytesPerSample == 2) {
+#ifdef VS_TARGET_CPU_X86
+                    YadifmodSIMD<uint16_t, Vec8us, Vec8i, 8>((const uint16_t *)prev2pp, (const uint16_t *)prev2pn, (const uint16_t *)prevp2p, (const uint16_t *)prevp, (const uint16_t *)prevp2n, (const uint16_t *)srcpp, (const uint16_t *)srcpn, (const uint16_t *)nextp2p, (const uint16_t *)nextp, (const uint16_t *)nextp2n, (const uint16_t *)next2pp, (const uint16_t *)next2pn, (const uint16_t *)edeintp, (uint16_t *)dstp, width, starty, stopy, stride3 >> 1, d);
+#else
                     YadifmodC<uint16_t>((const uint16_t *)prev2pp, (const uint16_t *)prev2pn, (const uint16_t *)prevp2p, (const uint16_t *)prevp, (const uint16_t *)prevp2n, (const uint16_t *)srcpp, (const uint16_t *)srcpn, (const uint16_t *)nextp2p, (const uint16_t *)nextp, (const uint16_t *)nextp2n, (const uint16_t *)next2pp, (const uint16_t *)next2pn, (const uint16_t *)edeintp, (uint16_t *)dstp, width, starty, stopy, stride3 >> 1, d);
+#endif
                 }
 
                 if (!fieldt)
